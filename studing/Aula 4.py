accb937533c52ed9bc8ffce0ab1e6ace39a76e67
zeorquejunior <zeroquejunior@gmail.com>
2021-07-04 16:33:26 -0300

    Aulas pro git

---
+++ studing/Aula 4.py
@@ -0,0 +1,21 @@
+#name=input("What's your name")
+#age=input("How old are you")
+#weight=input("How much you weight")
+#print("Name"+name,"Age"+age,"Weight"+weight)
+
+#Desafio 1 (curso em video)
+#name=input("What's your name?")
+#rint("Hello "+name+"!", "Nice to meet you!")
+
+#Desafio 2 (curso em video)
+# day = input("Day you were born?")
+# month = input("Month you were born?")
+# year = input("Year you were born?")
+# print("You were born at "+month, day ,"of "+year)
+
+#Desafio 3 (curso em video)
+# number1 = int(input("Type number 1:"))
+# number2 = int(input("Type number 2:"))
+# print(number1+number2)
+
+
